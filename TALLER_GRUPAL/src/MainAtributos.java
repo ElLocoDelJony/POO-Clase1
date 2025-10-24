31636eb312ea8c7c4f9ed36fa116b6746ea05d96
ElLocoDelJony <alexade2003v@gmail.com>
2025-10-23 22:19:22 -0500

    Delete TALLER_GRUPAL

--- TALLER_GRUPAL/src/MainAtributos.java
+++
@@ -1,8 +0,0 @@
-public class MainAtributos {
-    public static void main(String[] args){
-        Atributos a1 = new Atributos("Kevin",22);
-        a1.setNombre("Alex");
-        System.out.println(a1.getNombre());
-        System.out.println(a1.getEdad());
-    }
-}
